6cd4e32d721223d13be0ed1c116b6ea1f7f190df
aakarsh_jain <79127044+aakarshjaindev@users.noreply.github.com>
2025-05-03 03:55:37 +0530

    some changes

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,12 @@
+# # string concatenation (aka how to put strings together)
+# # suppose we want to create a string that says "subscribe to _____ "
+# youtuber = "Kylie Ying" # some string variable
+
+# # a few ways to do this
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
